c18489eeea89a73b62d945490635e099cd4a212d
Valentin <valentin.rogister@gmail.com>
2025-12-01 18:15:10 +0100

    cleaning

--- day01/part1.ml
+++ day01/part1.ml
@@ -3,12 +3,12 @@ let get_raw =
   |> Seq.filter (fun c -> c != '\r')
   |> String.of_seq
 
+let data =
+  get_raw |> String.split_on_char '\n'
+  |> List.map (fun s ->
+         (s.[0], String.sub s 1 (String.length s - 1) |> int_of_string))
+
 let () =
-  let data =
-    get_raw |> String.split_on_char '\n'
-    |> List.map (fun s ->
-           (s.[0], String.sub s 1 (String.length s - 1) |> int_of_string))
-  in
   let rec f n acc = function
     | (d, nn) :: tl ->
         let nn = match d with 'L' -> -nn | 'R' -> nn | _ -> assert false in
